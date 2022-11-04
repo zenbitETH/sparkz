e8527fce0ce1bb40a2f4facd1d26e59eb0baa7fa
Casey <leptone55@gmail.com>
2022-11-03 23:57:04 -0700

    google map tiles

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -40,7 +40,8 @@ const Map = ({ position }: { position: LatLngTuple }) => {
       <GestureHandlingSetter />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url='http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+        subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
       />
       <Marker position={position}>
         <Popup>
