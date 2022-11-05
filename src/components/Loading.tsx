99ab71e94c641f7889c3d6cefe49a456b0463977
Casey <leptone55@gmail.com>
2022-11-05 16:50:57 -0700

    Optimize loading animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
 import loadingAnimation from '../../public/loading.gif'
+import paintedLadies from '../../public/paintedLadies.webp'
 
 export default function Loading() {
   return (
-    <div
-      className='flex h-screen items-center justify-center bg-[url("/paintedLadies.webp")]
-    bg-cover bg-center bg-no-repeat
-		backdrop-blur-lg'
-    >
+    <div className='flex h-screen items-center justify-center'>
+      <Image
+        className='absolute z-[-1]'
+        src={paintedLadies}
+        alt='Painted Ladies'
+        fill
+        style={{
+          //filter: 'blur(10px)',
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}
+      />
       <div className='w-4/12 rounded-full border-[18px] border-[#8f4ee1] p-20'>
         <Image
           src={loadingAnimation}
