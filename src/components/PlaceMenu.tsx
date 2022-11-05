e0d0bf2dd57240687df06b86454bcd5531a810a9
HabacucMX <habacuc.vera@gmail.com>
2022-11-05 01:32:45 -0700

    Style updated on Place Menu

--- src/components/PlaceMenu.tsx
+++ src/components/PlaceMenu.tsx
@@ -3,16 +3,21 @@ interface Props {
 }
 const PlaceMenu = ({ name }: Props) => {
   return (
-    <div className='w-1000 capitalize text-white'>
+    <div className='w-1000 capitalize text-white grid gap-5 font-exo'>
       {console.log('name', name)}
-      <h2>{name}</h2>
+      <div className="text-xl text-center">{name}</div>
       {/* Buttons Row */}
-      <div>
-        <button className='border'>Ride</button>
-        <button className='border'>Move</button>
-        <button className='border'>Attack</button>
+      <div className="grid grid-cols-3 gap-5 w-full text-center">
+        <button className='border w-full rounded-xl lg:text-lg px-5 py-3'>Ride</button>
+        <button className='border w-full rounded-xl lg:text-lg px-5 py-3'>Move</button>
+        <button className='border w-full rounded-xl lg:text-lg px-5 py-3'>Attack</button>
       </div>
-      <button>click me</button>
+      <div className="grid grid-cols-2 text-center text-lg"> 
+        <div className=' '> 1245 ✨</div>
+        <div className=' '> 123 🌑</div>
+        
+      </div>
+      <div className="text-center text-lg">owner: 0x1345...abc</div>
     </div>
   )
 }
