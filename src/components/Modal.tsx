95709a76969ed7ff00b58003c6c6a4d879fdd455
Casey <leptone55@gmail.com>
2022-11-04 23:48:14 -0700

    Remove toggle from Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,7 @@
 export default function Modal(props) {
   return (
     <div id='modal' className='absolute z-50 flex h-20 w-full justify-center'>
-      <div
-        onClick={props.handleMarker}
-        className='flex w-20 justify-center bg-green-500'
-      >
-        {' '}
-        Modal
-      </div>
+      <div className='flex w-20 justify-center bg-green-500'> Modal</div>
     </div>
   )
 }
