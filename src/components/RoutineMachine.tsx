bc68f76561b88b2180eba1afc6a41df9b65e3c8d
Casey <leptone55@gmail.com>
2022-11-05 20:31:09 -0700

    RoutineMachine

---
+++ src/components/RoutineMachine.tsx
@@ -0,0 +1,30 @@
+import L from 'leaflet'
+import { createControlComponent } from '@react-leaflet/core'
+import 'leaflet-routing-machine'
+
+const createRoutineMachineLayer = ({ startPoint, endPoint }) => {
+  const instance = L.Routing.control({
+    waypoints: [
+      L.latLng(startPoint[0], startPoint[1]),
+      L.latLng(endPoint[0], endPoint[1]),
+    ],
+    lineOptions: {
+      styles: [{ color: 'red', weight: 4 }],
+    },
+    show: false,
+    addWaypoints: false,
+    routeWhileDragging: false,
+    draggableWaypoints: true,
+    fitSelectedRoutes: true,
+    showAlternatives: false,
+    createMarker: function () {
+      return null
+    },
+  })
+
+  return instance
+}
+
+const RoutingMachine = createControlComponent(createRoutineMachineLayer)
+
+export default RoutingMachine
