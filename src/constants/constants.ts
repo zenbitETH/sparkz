bd53226ccaf42541894d5c7a1393bf73f39c51ac
donpdang <pddang@ucalgary.ca>
2022-11-06 01:36:11 -0800

    add smart contract calls for ride

---
+++ src/constants/constants.ts
@@ -0,0 +1,74 @@
+export const locationMapping = {
+    "0xb48d1e7f3ec2cd61200b75007868508ed4915c39835aea15771d52b5e5af2c1": {
+        lat: "3780295",
+        long: "12244844"
+    },
+    "0x5ea9401191d9964703720acb4a47f178c767116c1b91177ef5b15903eec951c": {
+        lat: "3780820",
+        long: "12247502"
+    },
+    "0x2a8d9535cccb885152c78c8f340303c11273d2b48687cca8ade18149997843f": {
+        lat: "3782603",
+        long: "12247865"
+    },
+    "0x1bcb0ef6c288b1bf6bbac837419a22a7e492de74344ec71aa31497a8168f8b3": {
+        lat: "3780389",
+        long: "12246508"
+    },
+    "0x24c0e98e1ebac9b8e79b4b6233fbcf932cb1fcdc2eb1a3af4db15887437667f": {
+        lat: "3776823",
+        long: "12248568"
+    },
+    "0xe1045ad132b775f241ee8159e77a14e30f4550fc146a8fc5bbf0a6611d2cf552": {
+        lat: "3776863",
+        long: "12244169"
+    },
+    "0x36c4747008207646e318ccb05c34c4756943c361e7f1764b7a2ef5b10da8f3e8": {
+        lat: "3776810",
+        long: "12238815"
+    },
+    "0x63069d28d80fff34b89f0564411f6109fd5923642bd3bc830ffc2371b0aab285": {
+        lat: "3777811",
+        long: "12241731"
+    },
+    "0x505b816c0a64a2273862b369bfb8a76f92bff7257c46532af5ebff297d91b81a": {
+        lat: "3778572",
+        long: "12240108"
+    },
+    "0x1c9ba10f30d5cd344085aaf70937381c98385a531f12bf0433342e8b6019534a": {
+        lat: "3775986",
+        long: "12251099"
+    },
+    "0x6d0476b537047d1e769a7b15a18dc495893290f46fd8ca311cd74a6b1e46d4a1": {
+        lat: "3779381",
+        long: "12248357"
+    },
+    "0x9505665f3c0ccc17ad1036f8bc0cd79841e65f81bec6d743d300dea085e37830": {
+        lat: "3778975",
+        long: "12239720"
+    },
+    "0x8a84a5d38f233d6125aa553bb6d70f08969dd1bd9d27aef8f4f87cad4e2fbd45": {
+        lat: "3777858",
+        long: "12238957"
+    },
+    "0x00bf3a345d11e5e493d1129ac0ae4cc897772ce8d35d09285fe1e289ecd72cad": {
+        lat: "3779540",
+        long: "12239363"
+    },
+    "0xd8e4c7967f5077636acfa74c027ed0e0f1eaad838a9cc7c9540949d6f2472a01": {
+        lat: "3775986",
+        long: "12242718"
+    },
+    "0xff5613276a1676d80dc84bd1501c48601093d2c43c1cdc70e0ffb0205d27ce00": {
+        lat: "3773328",
+        long: "12250528"
+    },
+    "0x10190a7c03c84889b98f3d8c060621fc1f3a92cbd8490b7ea53b44c56d98125a": {
+        lat: "3777764",
+        long: "12242053"
+    },
+    "0xa219ea8b9989096cb5c1fbefadf695cab7367d31e3fad7d7b5329447aadf57cb": {
+        lat: "3780875",
+        long: "12241251"
+    },
+}
