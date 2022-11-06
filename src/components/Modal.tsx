dbdb47a473e3f9099cc7af57654cfc0df31a1d62
donpdang <pddang@ucalgary.ca>
2022-11-05 17:12:58 -0700

    move images to public

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export default function Modal(props) {
   return (
     <div id='modal' className='bg-purple-500 text-lg  lg:text-2xl text-white font-exo font-bold fixed left-1/2 transform -translate-x-1/2 grid grid-cols-3 z-50 h-20 lg:w-1/2  w-full rounded-b-xl text-center items-center'>
-      <Link href="/admin/addPlace"><div className=' '> 1245 ✨</div></Link>
-      <div className=' '> 123 🌑</div>
-      <div className=' '> Solarpunk ☀️</div>
+      <div className=' '> 1245 ✨</div>
+      <div className  =' '> 123 🌑</div>
+      <Link className=" border border-gray-400 text-base mr-4 rounded-full" href="/admin/addPlace">Add Place</Link>
     </div>
   )
 }
