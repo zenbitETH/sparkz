b52fe1ab17bda4999792a428d00007fb8e25ce78
donpdang <pddang@ucalgary.ca>
2022-11-06 05:00:58 -0800

    get spark from on chain

--- src/components/TopModal.tsx
+++ src/components/TopModal.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link'
+import { useAccount, useContractRead } from 'wagmi'
+import sparkZjson from '../constants/sparkZ.json'
 
 export default function Modal(props) {
+  const { address, isConnected } = useAccount()
+  const { data, isError, isLoading } = useContractRead({
+    address: '0xd66a0156935684bd2b1Cb6a2aBE9c6B1c26b94CA',
+    abi: sparkZjson.abi,
+    functionName: 'addressToUserDetail',
+    args:[address]
+  })
   return (
     <div
       id='modal'
       className='fixed left-1/2  z-50 grid h-20 w-full -translate-x-1/2 transform grid-cols-4 items-center rounded-b-xl bg-purple-500 text-center font-exo text-lg  font-bold text-white lg:w-1/2 lg:text-2xl'
     >
-      <div className=' '> 1245 ✨</div>
+            {data && data.sparkz && <div className=' '>{`${data.sparkz}  ✨`}</div>}
+            {data && data.shadowz && <div className=' '>{`${data.shadowz}  🌑`}</div>}
       <div className=' '> 123 🌑</div>
       <Link
         className=' mr-4 rounded-full border border-gray-400 text-base'
