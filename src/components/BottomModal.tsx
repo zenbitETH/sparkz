bd1daa87e2810354bd53ce00c7e3348b2a364827
Casey <leptone55@gmail.com>
2022-11-06 03:07:05 -0800

    Seems to be working: writing somthing to chain

--- src/components/BottomModal.tsx
+++ src/components/BottomModal.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { usePrepareContractWrite, useContractWrite } from 'wagmi'
 import { type RideState } from './Map'
+import sparkZData from '../../artifacts/contracts/Sparkz.sol/SparkZ.json' assert { type: 'json' }
 
 //type Dictionary = keyof RideState
 interface Dictionary {
@@ -81,34 +82,42 @@ export default function Modal({
   const rideEndTime = Math.floor(rideStartTime + routeLength / BIKE_SPEED)
 
   const payload = [
-    startPointId,
+    1,
+    startPointId - 1,
     rideStartTime,
-    endPointId,
+    endPointId - 1,
     rideEndTime,
-    routeLength,
-    routeLength,
+    parseInt(routeLength),
+    parseInt(routeLength),
   ]
 
   const { config } = usePrepareContractWrite({
     address: '0xd66a0156935684bd2b1Cb6a2aBE9c6B1c26b94CA',
-    abi: [],
+    abi: sparkZData.abi,
     functionName: 'registerJourney',
-    args: payload,
+    args: payload.map((arg) => arg?.toString()),
     chainId: 80001,
   })
+
+  console.log('==============================')
+  console.log('==============================')
+  console.log('==============================')
+  console.log('==============================')
+  console.log(payload)
 
   const {
     data: contractWriteData,
     isLoading: isWriteLoading,
     isSuccess,
+    Error,
     write,
   } = useContractWrite(config)
-
+  console.log('')
   return (
     <div
       id='modal'
       className={`${bgColor} ${cursor} fixed left-1/2 bottom-0 z-50 grid h-20 w-full -translate-x-1/2 transform items-center rounded-t-xl bg-purple-500 text-center font-exo text-lg  font-bold text-white lg:w-1/2 lg:text-2xl`}
-      onClick={() => {
+      onClick={async () => {
         if (rideState === 'rideChosen') {
           const closeEnough = within20m()
           if (closeEnough) {
@@ -125,8 +134,9 @@ export default function Modal({
             rideStartTime + routeLength / BIKE_SPEED
           )
           setRideEndTime(rideEndTime)
-
-          write?.()
+          console.log('55555555555555555555555555555555555555555555555555')
+          console.log('about to write')
+          await write?.()
         }
       }}
     >
